873338c39cd786f560b3f28c4bfc5ab141d9a623
mansi bhokarikar <mansibhokarikar62@gmail.com>
2024-12-25 12:39:33 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Contact Form Submission
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for your message, Mansi will get back to you soon!");
+    this.reset();
+  });
+  
